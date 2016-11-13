44c9db5a1d9662b1dd70055da413011bdaa2bd8b
Raymond Crawford <Raymond Crawford>
2016-11-13 00:04:13 -0800

    ChessBoardImpl

---
+++ src/com/zynpo/impls/ChessBoardImpl.java
@@ -0,0 +1,54 @@
+package com.zynpo.impls;
+
+import com.zynpo.interfaces.*;
+
+public class ChessBoardImpl implements ChessBoard {
+
+    private ChessSquare[] _squares;
+
+    public int getRowCount() { return 8; }
+    public int getColCount() { return 8; }
+
+    public ChessBoardImpl() {
+        _squares = new ChessSquare[getRowCount() * getColCount()];
+
+        for (int row = 0; row < getRowCount(); ++row) {
+            for (int col = 0; col < getColCount(); ++col) {
+                int i = row * getColCount() + col;
+                _squares[i] = new ChessSquareImpl(this, row, col);
+            }
+        }
+    }
+
+
+    public boolean squareExists(int row, int col) {
+        return (0 <= row) &&
+                (row < getRowCount()) &&
+                (0 <= col) &&
+                (col < getColCount());
+    }
+
+
+    public ChessSquare getSquare(int row, int col) {
+        if (squareExists(row, col))
+            return _squares[row * getColCount() + col];
+        else
+            return null;
+    }
+
+
+    public ChessSquare getSquare(String notation) {
+        notation = notation.trim().toLowerCase();
+        
+        if (notation.length() != 2)
+            throw new IllegalArgumentException("Unexpected square notation: " + notation);
+
+        char[] chars = notation.toCharArray();
+
+        int col = (int) (chars[0] - 'a');
+        int row = (int) (chars[1] - '1');
+
+        return getSquare(row, col);
+    }
+
+}
