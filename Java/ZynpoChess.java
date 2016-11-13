04c555fb715a1114978d573f36906f72d65d26c6
Raymond Crawford <Raymond Crawford>
2016-11-12 17:17:07 -0800

    Initial ZynpoChess.java with void main() { Hello World! }

---
+++ Java/ZynpoChess.java
@@ -0,0 +1,12 @@
+
+public class ZynpoChess {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        for(int i = 0; i < args.length; ++i) {
+            System.out.println(String.format("args[%d] = %s", i, args[i]));
+        }
+    }
+
+}
